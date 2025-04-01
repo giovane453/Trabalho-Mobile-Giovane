a4cb1675e443b2204f400cfbbe762dc2aec82ede
giovane453 <giovane.campanha@escola.pr.gov.br>
2025-04-01 12:26:18 -0300

    first

--- Trabalho-Giovane-Mobile/app/(tabs)/explore.tsx
+++ Trabalho-Giovane-Mobile/app/(tabs)/explore.tsx
@@ -12,15 +12,13 @@ export default function TabTwoScreen() {
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
+        <Image
+        source={require('@/assets/images/principal.webp')}
+        style={styles.headerImage}
+      />
+    }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Allen the Alien gallery</ThemedText>
       </ThemedView>
       <ThemedText>This app includes example code to help you get started.</ThemedText>
       <Collapsible title="File-based routing">
@@ -97,11 +95,14 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
+    height: 250,
+    width: 400,
+    bottom: 0,
+    left: 0,
     position: 'absolute',
   },
+
+
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
